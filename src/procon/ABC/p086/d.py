846ae0ebd33ed91fa9915d2743b819ad50d1809b
sakuv2 <loots2438@gmail.com>
2021-03-21 14:31:05 +0900

    p086d

--- src/procon/ABC/p086/d.py
+++ src/procon/ABC/p086/d.py
@@ -1,4 +1,5 @@
 # https://atcoder.jp/contests/abc086/tasks/arc089_b
+# https://atcoder.jp/contests/abc086/submissions/21124856
 from itertools import product
 from typing import List, Tuple
 
@@ -16,17 +17,17 @@ def get_inputs() -> Tuple[int, List[Tuple[int, int, int]]]:
 def main():
     k, steps = get_inputs()
 
-    g = [[[0, 0] for _ in range(k)] for _ in range(k)]
+    g = [[[0] * k for _ in range(k)] for _ in range(2)]
     for step in steps:
         x, y, c = step
         x, y = x % (2 * k), y % (2 * k)
         c = 1 - c if (x < k) ^ (y < k) else c
         x, y = x if x < k else x - k, y if y < k else y - k
-        g[x][y][c] += 1
+        g[c][x][y] += 1
 
     cs = [[[0] * (k + 1) for _ in range(k + 1)] for _ in range(2)]
     for x, y, c in product(range(k), range(k), (0, 1)):
-        cs[c][x + 1][y + 1] = cs[c][x + 1][y] + cs[c][x][y + 1] - cs[c][x][y] + g[x][y][c]
+        cs[c][x + 1][y + 1] = cs[c][x + 1][y] + cs[c][x][y + 1] - cs[c][x][y] + g[c][x][y]
 
     ans = 0
     for x, y in product(range(k), range(k)):
